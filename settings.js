53b1c1f51315448b1f5d01db674218c8750b1ccf
bilalnadeem3149-sketch <bilalnadeem3149@gmail.com>
2026-05-04 18:17:47 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'BILAL KING',
+  author: 'BILAL',
+  botName: "BILAL MD",
+  botOwner: 'Dev BILAL', // Your name
+  ownerNumber: '923078071982', //Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.7.5",
+};
+
+module.exports = settings;
