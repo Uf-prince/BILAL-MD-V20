d6ae8d0ff30e4fdf20118a4e9b37f273d18ef3fc
bilalnadeem3149-sketch <bilalnadeem3149@gmail.com>
2026-05-04 18:26:28 +0500

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,13 +1,56 @@
-const settings = {
-  packname: 'BILAL KING',
-  author: 'BILAL',
-  botName: "BILAL MD",
-  botOwner: 'Dev BILAL', // Your name
-  ownerNumber: '923078071982', //Set your number here without + symbol.
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
-  version: "2.7.5",
-};
+const fs = require('fs')
 
-module.exports = settings;
+//~~~~~~~~~~~ Settings Owner ~~~~~~~~~~~//
+global.owner = "923078071982"
+global.developer = "923078071982"
+global.bot = ""
+global.devname = "𝗕𝗜𝗟𝗔𝗟 𝐊𝐈𝐍𝐆"
+global.ownername = "𝗕𝗜𝗟𝗔𝗟 𝐊𝐈𝐍𝐆"
+global.botname = "Lady bella"
+global.versisc = "2"
+global.packname = "⎋Lady bella"
+//~~~~~~~~~~~ Settings Sosmed ~~~~~~~~~~~//
+global.linkwa = "https://wa.me/923078071982"
+global.linktele = "https://t.me"
+
+//~~~~~~~~~~~ Settings Bot ~~~~~~~~~~~//
+global.prefix = ["","!",".",",","#","/","🎭","〽️"]
+global.autoRecording = false
+global.autoTyping = false 
+global.autorecordtype = false
+global.autoread = true
+global.autobio = false
+global.anti92 = false
+global.owneroff = false
+global.autoswview = true
+global.autoreaction = true
+
+//~~~~~~~~~~~ Settings Thumbnail ~~~~~~~~~~~//
+global.thumbbot = "https://files.catbox.moe/e16jwg.bin"
+global.thumbown = "https://files.catbox.moe/e16jwg.bin"
+
+//~~~~~~~~~~~ Settings Channel ~~~~~~~~~~~//
+global.idchannel = "120363406434037642@newsletter*"
+global.channelname = "BILAL-MD ❮SUPPORT❯"
+global.channel = "https://whatsapp.com/channel/0029Vaj3Xnu17EmtDxTNnQ0G"
+
+//~~~~~~~~~~~ Settings Message ~~~~~~~~~~~//
+global.mess = {
+  developer: " `[ Developer Only!! ]` \n This feature is for developers only!!",
+  owner: " `[ Owner Only!! ]` \n This feature is for owners only!!",
+  group: " `[ Group Only!! ]` \n This feature is for group chats only!!",
+  private: " `[ Private Only!! ]` \n This feature is for private chats only!!",
+  admin: " `[ Admin Only!! ]` \n This feature is for admins only!!",
+  botadmin: " `[ Bot Admin Only!! ]` \n This feature is for bot admins only!!",
+  wait: " `[ Wait!! ]` \n Please wait, loading...",
+  error: " `[ Error!! ]` \n An error occurred!!",
+  done: " `[ Done!! ]` \n completed ✅!!"
+}
+
+let file = require.resolve(__filename)
+require('fs').watchFile(file, () => {
+  require('fs').unwatchFile(file)
+  console.log('\x1b[0;32m'+__filename+' \x1b[1;32mupdated!\x1b[0m')
+  delete require.cache[file]
+  require(file)
+})
