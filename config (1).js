7b35a3615244bf7cc4532c68bf7813b4f35d96d1
UMAR • FAROOQ <ufprince1@gmail.com>
2026-05-15 18:34:01 +0500

    Add files via upload

---
+++ config (1).js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+global.APIs = {
+    xteam: 'https://api.xteam.xyz',
+    dzx: 'https://api.dhamzxploit.my.id',
+    lol: 'https://api.lolhuman.xyz',
+    violetics: 'https://violetics.pw',
+    neoxr: 'https://api.neoxr.my.id',
+    zenzapis: 'https://zenzapis.xyz',
+    akuari: 'https://api.akuari.my.id',
+    akuari2: 'https://apimu.my.id',
+    nrtm: 'https://fg-nrtm.ddns.net',
+    bg: 'http://bochil.ddns.net',
+    fgmods: 'https://api-fgmods.ddns.net'
+};
+
+global.APIKeys = {
+    'https://api.xteam.xyz': process.env.APIKEY_XTEAM || 'd90a9e986e18778b',
+    'https://api.lolhuman.xyz': process.env.APIKEY_LOL || '85faf717d0545d14074659ad',
+    'https://api.neoxr.my.id': 'yourkey',
+    'https://violetics.pw': 'beta',
+    'https://zenzapis.xyz': 'yourkey',
+    'https://api-fgmods.ddns.net': 'fg-dylux'
+};
+
+module.exports = {
+    WARN_COUNT: 1, 
+    // Aapka Naya MongoDB Atlas Link
+    MONGODB: process.env.MONGODB_URL || 'mongodb+srv://bilal_botz:abcd234@cluster0.rvcbg6h.mongodb.net/BILAL_MD?retryWrites=true&w=majority',
+    APIs: global.APIs,
+    APIKeys: global.APIKeys
+};
